4396de8cbdcbb97eced09e21d23bcec9a96d9d61
Christian Herlemann <christian.herlemann@develappers.de>
2022-03-14 13:58:29 +0100

    Adding popToRoot

--- Sources/Stinsen/TabCoordinatable/TabCoordinatable.swift
+++ Sources/Stinsen/TabCoordinatable/TabCoordinatable.swift
@@ -77,6 +77,9 @@ public extension TabCoordinatable {
                         },
                         tabItem: { [unowned self] in
                             val.tabItem(active: $0, coordinator: self)
+                        },
+                        onPopToRoot: {
+                            val.onPopToRoot(coordinator: self)
                         }
                     )
                 )
